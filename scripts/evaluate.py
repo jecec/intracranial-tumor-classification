979cd6ca423ca44fc15695e39a151ead25e2c3ca
Jere <jere.paloheimo@gmail.com>
2025-10-21 18:29:22 +0300

    Fixed 'evaluate' and added metric collection for loss, accuracy, balanced_accuracy, macro_f1, per_label_f1 and per_label_roc_auc

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 from pathlib import Path
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import *
 
 from args import get_args
 from model import PreTrainedModel
@@ -13,27 +14,45 @@ device = args.device
 
 def evaluate(test_loader):
     """Main evaluation function"""
+    criterion = nn.CrossEntropyLoss()
+    fold_metrics = []
     for path in Path(args.model_dir).iterdir():
         model = PreTrainedModel(args.backbone, pretrained=False).to(device)
         model.load_state_dict(torch.load(path, weights_only=True))
-        all_preds, all_targets = [], []
-        for batch in test_loader:
-            inputs = batch['img'].to(device)
-            targets = batch['label'].to(device)
 
-            outputs = model(inputs)
-            preds = torch.argmax(outputs, dim=1)
+        test_loss = 0
+        all_targets, all_preds, all_probs = [], [], []
 
-            all_preds.append(preds.cpu().numpy())
-            all_targets.append(targets.cpu().numpy())
+        with torch.no_grad():
+            for batch in test_loader:
+                inputs = batch['img'].to(device)
+                targets = batch['label'].to(device)
+                outputs = model(inputs)
 
-        preds = np.concatenate(all_preds, axis=0)
+                loss = criterion(outputs, targets)
+                test_loss += loss.item()
+
+                all_targets.append(targets.detach().cpu().numpy())
+                all_preds.append(torch.argmax(outputs, dim=1).detach().cpu().numpy())
+                all_probs.append(torch.softmax(outputs, dim=1).detach().cpu().numpy())
+
         targets = np.concatenate(all_targets, axis=0)
+        preds = np.concatenate(all_preds, axis=0)
+        probs = np.concatenate(all_probs, axis=0)
 
-        # TODO: Add more metrics for evaluation
+        metrics = {
+            "loss": test_loss / len(test_loader),
+            "accuracy": accuracy_score(targets, preds),
+            "balanced_accuracy": balanced_accuracy_score(targets, preds),
+            "macro_f1": f1_score(targets, preds, average="macro"),
+            "per_label_f1": f1_score(targets, preds, average=None),
+            "roc_auc_macro": roc_auc_score(targets, probs, multi_class="ovr", average="macro"),
+            "per_label_roc_auc": roc_auc_score(targets, probs, multi_class="ovr", average=None),
+            "confusion_matrix": confusion_matrix(targets, preds)
+        }
 
-        cm = confusion_matrix(targets, preds)
-        ConfusionMatrixDisplay(cm).plot()
-        plt.show()
+        fold_metrics.append(metrics)
+
+    return fold_metrics
 
 
