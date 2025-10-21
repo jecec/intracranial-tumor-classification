573ede050d8d10e52e48aedf99e742ec03c9fea6
Jere <jere.paloheimo@gmail.com>
2025-10-21 11:46:19 +0300

    Added evaluation functionality to main.py and evaluate.py
    Currently only confusion matrix for evaluation

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,1 +1,39 @@
-# TODO Add functionality for model evaluation with the test set
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from pathlib import Path
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+from args import get_args
+from model import PreTrainedModel
+
+args = get_args()
+device = args.device
+
+def evaluate(test_loader):
+    """Main evaluation function"""
+    for path in Path(args.model_dir).iterdir():
+        model = PreTrainedModel(args.backbone, pretrained=False).to(device)
+        model.load_state_dict(torch.load(path, weights_only=True))
+        all_preds, all_targets = [], []
+        for batch in test_loader:
+            inputs = batch['img'].to(device)
+            targets = batch['label'].to(device)
+
+            outputs = model(inputs)
+            preds = torch.argmax(outputs, dim=1)
+
+            all_preds.append(preds.cpu().numpy())
+            all_targets.append(targets.cpu().numpy())
+
+        preds = np.concatenate(all_preds, axis=0)
+        targets = np.concatenate(all_targets, axis=0)
+
+        # TODO: Add more metrics for evaluation
+
+        cm = confusion_matrix(targets, preds)
+        ConfusionMatrixDisplay(cm).plot()
+        plt.show()
+
+
--- scripts/main.py
+++ scripts/main.py
@@ -5,14 +5,19 @@ import os
 
 from dataset import prepare_dataset, k_fold_cv, MRI_dataset
 from args import get_args
-from model import preTrainedModel
+from model import PreTrainedModel
 from trainer import train
+from evaluate import evaluate
+from utils import setup_project_dirs
 
 def main():
 
     # Fetch arguments and get device
     args = get_args()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = args.device
+
+    # Prepare folder structure
+    setup_project_dirs()
 
     ### PREPARE CSVs ###
     # Storing dataset into train and test csv files
@@ -20,13 +25,14 @@ def main():
     # Creating stratified 5-fold splits
     k_fold_cv(f'{args.csv_dir}/train_metadata.csv')
 
-    ### ITERATE AMONG THE FOLDS ###
-    for fold in range(args.n_folds):
-        print(f"Training on fold {fold + 1} out of {args.n_folds}")
+    ### ITERATE AMONG THE FOLDS AND TRAIN THE MODEL ###
+    for fold in range(args.folds):
+        print(f"\nTraining on fold {fold + 1} out of {args.folds}")
 
         # 1. Load datasets
         train_set = pd.read_csv(os.path.join(args.csv_dir, f'fold_{fold}_train.csv'))
         val_set = pd.read_csv(os.path.join(args.csv_dir, f'fold_{fold}_val.csv'))
+
 
         # 2. Prepare datasets
         train_dataset = MRI_dataset(train_set)
@@ -41,12 +47,16 @@ def main():
                                 prefetch_factor=args.pre_fetch, persistent_workers=True)
 
         # 4. Initialize the model
-        model = preTrainedModel(args.backbone).to(device)
+        model = PreTrainedModel(args.backbone, pretrained=True).to(device)
 
         # 5. Training the model
         train(model, train_loader, val_loader, fold)
 
-        # TODO: Add model evaluation
+    ## MODEL EVALUATION ##
+    test_set = pd.read_csv(f'{args.csv_dir}/test_metadata.csv')
+    test_dataset = MRI_dataset(test_set)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+    evaluate(test_loader)
 
 
 if __name__ == '__main__':
