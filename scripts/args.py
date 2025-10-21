859cb238c4cfab99160391b56e0f123e508d0eb2
Jere <jere.paloheimo@gmail.com>
2025-10-21 22:48:37 +0300

    Added arguments for -checkpoint_dir and -resume for loading checkpoints

--- scripts/args.py
+++ scripts/args.py
@@ -10,21 +10,23 @@ def get_args():
     data_group.add_argument('-output_dir', type=str, default='output')
     data_group.add_argument('-model_dir', type=str, default='output/models')
     data_group.add_argument('-visual_dir', type=str, default='output/visuals')
+    data_group.add_argument('-checkpoint_dir', type=str, default='output/checkpoint.pth')
 
-    # Training
+    # Training hyperparameters
     train_group = parser.add_argument_group('Training Options')
     train_group.add_argument('-batch_size', type=int, default=32, choices=[16, 24, 32])
     train_group.add_argument('-num_workers', type=int, default=8, choices=[4, 6, 8])
     train_group.add_argument('-pre_fetch', type=int, default=4, choices=[1, 2, 4])
-    train_group.add_argument('-epochs', type=int, default=5, choices=[25, 50, 100])
+    train_group.add_argument('-epochs', type=int, default=75, choices=[50, 75, 100])
     train_group.add_argument('-lr', type=float, default=1e-3, choices=[1e-3, 1e-4, 1e-5])
     train_group.add_argument('-folds', type=int, default=5)
 
-    # Backbone, seed and device
+    # Miscellaneous options such as resume for resuming training from saved checkpoints
     misc_group = parser.add_argument_group('Miscellaneous')
-    misc_group.add_argument('-backbone', type=str, default='resnet18', choices=['resnet18', 'resnet50'])
+    misc_group.add_argument('-backbone', type=str, default='resnet34', choices=['resnet18', 'resnet34', 'resnet50'])
     misc_group.add_argument('-seed', type=int, default=28)
     misc_group.add_argument('-device', type=str, default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    misc_group.add_argument('-resume', type=bool, default=False, choices=[True, False])
 
     args = parser.parse_args()
     return args
