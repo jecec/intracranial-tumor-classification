74e811c08532f879c70ebe62d6cef8bb06785bf5
Jere <jere.paloheimo@gmail.com>
2025-10-31 02:22:57 +0200

    Created setup.py script for setting up the required project structure.

---
+++ scripts/setup.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from args import get_args
+
+args = get_args()
+folders = [
+        Path(args.csv_dir),
+        Path(args.output_dir),
+        Path(args.model_dir),
+        Path(args.visual_dir),
+        Path(args.checkpoint_dir),
+        Path(args.metrics_dir),
+        Path("data"),
+    ]
+for folder in folders:
+    folder.mkdir(parents=True, exist_ok=True)
