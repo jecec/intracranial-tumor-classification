c4381ea0d12e4137371b7f3ac55a6b17d24eaee1
Jere <jere.paloheimo@gmail.com>
2025-10-31 02:37:19 +0200

    Added arguments for skipping different steps of the model training and evaluation
    -train_cv toggles training of k-fold-cv
    -train_main toggles training of final singular model
    -evaluate toggles evaluation of saved final model

--- scripts/args.py
+++ scripts/args.py
@@ -1,23 +1,25 @@
 import argparse
 import torch
 def get_args():
+    # TODO: Split arguments to config.yaml and argparser for usability
     parser = argparse.ArgumentParser('Model Training Arguments')
 
     # File paths
     data_group = parser.add_argument_group('Data Options')
     data_group.add_argument('-data', type=str, default='data/brisc2025/classification_task')
-    data_group.add_argument('-csv_dir', type=str, default='CSVs')
+    data_group.add_argument('-csv_dir', type=str, default='data/CSVs')
     data_group.add_argument('-output_dir', type=str, default='output')
     data_group.add_argument('-model_dir', type=str, default='output/models')
     data_group.add_argument('-visual_dir', type=str, default='output/visuals')
-    data_group.add_argument('-checkpoint_dir', type=str, default='output/checkpoint.pth')
+    data_group.add_argument('-checkpoint_dir', type=str, default='output/checkpoints')
+    data_group.add_argument('-metrics_dir', type=str, default='output/metrics')
 
     # Training hyperparameters
     train_group = parser.add_argument_group('Training Options')
     train_group.add_argument('-batch_size', type=int, default=32, choices=[16, 24, 32])
     train_group.add_argument('-num_workers', type=int, default=8, choices=[4, 6, 8])
     train_group.add_argument('-pre_fetch', type=int, default=4, choices=[1, 2, 4])
-    train_group.add_argument('-epochs', type=int, default=10, choices=[5, 10, 15])
+    train_group.add_argument('-epochs', type=int, default=5, choices=[5, 10, 15])
     train_group.add_argument('-lr', type=float, default=1e-4, choices=[1e-3, 1e-4, 1e-5])
     train_group.add_argument('-folds', type=int, default=5)
 
@@ -29,6 +31,9 @@ def get_args():
     misc_group.add_argument('-device', type=str, default=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
     misc_group.add_argument('-resume', type=bool, default=False, choices=[True, False])
     misc_group.add_argument('-num_classes', type=int, default=4)
+    misc_group.add_argument('-train_cv', type=bool, default=True)
+    misc_group.add_argument('-train_main', type=bool, default=True)
+    misc_group.add_argument('-evaluate', type=bool, default=True)
 
     args = parser.parse_args()
     return args
