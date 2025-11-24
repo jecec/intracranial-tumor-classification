e325d753d370c757509bf8b675dd67b1511c8970
Jere <jere.paloheimo@gmail.com>
2025-11-24 23:43:51 +0200

    Added layer freezing logic and modified the custom classifier

--- scripts/model.py
+++ scripts/model.py
@@ -7,7 +7,6 @@ args = get_args()
 class PreTrainedModel(nn.Module):
     def __init__(self, backbone='resnet18', num_classes=4, pretrained=True):
         super(PreTrainedModel, self).__init__()
-
         # Select weights based on the pretrained flag
         if backbone == 'resnet18':
             weights = ResNet18_Weights.DEFAULT if pretrained else None
@@ -22,17 +21,27 @@ class PreTrainedModel(nn.Module):
             weights = ResNet101_Weights.DEFAULT if pretrained else None
             self.model = resnet101(weights=weights)
 
+        # Looking at layers to freeze conv1 (bn1, relu, maxpool, layer1, layer2, layer3, layer4, avgpool, fc
+        #for name, layer in self.model.named_children():
+        #    print(name)
+
+        # Freezing chosen model layers
+        for name, layer in self.model.named_children():
+            if name in ['conv1', 'bn1', 'layer1', 'layer2']:
+                for param in layer.parameters():
+                    param.requires_grad = False
+
         # Replace the final fully connected layer
         in_features = self.model.fc.in_features
         self.model.fc = nn.Identity()
 
         # Add a custom classification head
         self.classifier = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features, 256),
+            nn.Dropout(p=0.6),
+            nn.Linear(in_features, 128),
             nn.ReLU(),
-            nn.BatchNorm1d(256),
-            nn.Linear(256, num_classes)
+            nn.Dropout(p=0.6),
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
